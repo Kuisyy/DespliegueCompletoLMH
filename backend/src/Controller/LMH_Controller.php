313cab03815ca7cf53f7bcf9afe1f49410579908
Kuisyy <luis3valderrubio@gmail.com>
2025-02-04 13:16:21 +0100

    Update final

--- backend/src/Controller/LMH_Controller.php
+++ backend/src/Controller/LMH_Controller.php
@@ -1,27 +1,31 @@
 <?php
 namespace App\Controller;
+
 use Doctrine\DBAL\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
-class DbController extends AbstractController
+
+class LMH_Controller extends AbstractController 
 {
-private Connection $connection;
-public function __construct(Connection $connection)
-{
-$this->connection = $connection;
-}
-#[Route('/api/db', name: 'get_db')]
-public function index(): JsonResponse
-{
-// Consulta para recuperar el primer mensaje de la tabla "messages"
-$sql = 'SELECT fraseLMH FROM secretosLMH LIMIT 1';
-$result = $this->connection->fetchOne($sql);
-// Si no hay mensaje en la BD, devolver un mensaje de error
-if (!$result) {
-return $this->json(['fraseLMH' => 'No fraseLMH found in the database!']);
-}
-$result = 'Backend Operativo, respuesta de la BD: '.$result;
-return $this->json(['fraseLMH' => $result]);
-}
+    private Connection $connection;
+
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    #[Route('/api/db', name: 'get_db')]
+    public function index(): JsonResponse
+    {
+        $sql = 'SELECT fraseLMH FROM secretosLMH LIMIT 1';
+        $result = $this->connection->fetchOne($sql);
+
+        if (!$result) {
+            return $this->json(['fraseLMH' => 'No fraseLMH found in the database!']);
+        }
+
+        $result = 'Backend Operativo, respuesta de la BD: '.$result;
+        return $this->json(['fraseLMH' => $result]);
+    }
 }
